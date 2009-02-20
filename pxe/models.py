72e8fdfdb93523240b9e923989ef66afc0e4cf14
Frederic Lepied <frederic.lepied@splitted-desktop.com>
2009-02-20 08:41:06 +0100

    added an __str__ method

--- pxe/models.py
+++ pxe/models.py
@@ -11,19 +11,31 @@ from django.db import models
 
 class System(models.Model):
     name = models.CharField(max_length=100)
-
+    
+    def __str__(self):
+        return self.name
+    
 class MacAddress(models.Model):
     mac = models.CharField(max_length=12)
     system = models.ForeignKey(System)
     
+    def __str__(self):
+        return "%s %s" % (self.mac, self.system)
+
 class BootName(models.Model):
     name = models.CharField(max_length=100)
     available = models.BooleanField()
     
+    def __str__(self):
+        return self.name
+
 class Log(models.Model):
     date = models.DateTimeField(auto_now=True, auto_now_add=True)
     system = models.ForeignKey(System)
     boot_name = models.ForeignKey(BootName)
+
+    def __str__(self):
+        return "%s %s %s" % (self.date, self.system, self.boot_name)
 
 from django.contrib import admin
 admin.site.register(System)
